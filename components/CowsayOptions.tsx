969a8ed5dede342ef4af3cc026cca88e16509842
sbushman <steve@stevebushman.com>
2019-02-18 16:56:41 -0600

    Refactor options out

--- components/CowsayOptions.tsx
+++ components/CowsayOptions.tsx
@@ -1,155 +1,126 @@
-// import * as React from 'react'
+import * as React from 'react'
 
-// //import styles from './cowsayForm.scss'
-// const styles = require('./cowsayForm.scss')
+import styles from '../pages/cowsay.scss'
 
-// import Button from 'antd/lib/button'
-// import Input from 'antd/lib/input'
-// import Select from 'antd/lib/select'
-// const Option = Select.Option
-// import Form from 'antd/lib/form'
+import Layout from '../components/Layout'
 
-// interface IMode {
-//   key: string
-//   label: string
-// }
-// const modes: IMode[] = [
-//   {
-//     key: '',
-//     label: 'Normal',
-//   },
-//   {
-//     key: 'b',
-//     label: 'Borg',
-//   },
-//   {
-//     key: 'd',
-//     label: 'Dead',
-//   },
-//   {
-//     key: 'g',
-//     label: 'Greedy',
-//   },
-//   {
-//     key: 'p',
-//     label: 'Paranoid',
-//   },
-//   {
-//     key: 's',
-//     label: 'Stoned',
-//   },
-//   {
-//     key: 't',
-//     label: 'Tired',
-//   },
-//   {
-//     key: 'w',
-//     label: 'Wired',
-//   },
-//   {
-//     key: 'y',
-//     label: 'Youthful',
-//   },
-// ]
+import Button from 'antd/lib/button'
+import Input from 'antd/lib/input'
+const { TextArea } = Input
+import Select from 'antd/lib/select'
+const Option = Select.Option
+import Form from 'antd/lib/form'
 
-// class CowsayOptions extends React.Component<{ onChangeOptions }> {
-//   state = {
-//     text: '',
-//     mode: '',
-//   }
-//   _onChange_text = ev => {
-//     this.setState({ text: ev.target.value })
-//   }
-//   _onChange_mode = val => {
-//     this.setState({ mode: val })
-//   }
-//   render() {
-//     let { state } = this
-//     let { text, mode } = state
+import * as stateCowsay from '../state/stateCowsay'
+import { modes, actions } from '../state/stateCowsay'
 
-//     if (text.length === 0) {
-//       text = 'Moo'
-//     }
+export default class CowsayOptions extends React.Component<{}> {
+  _onChange_text = ev => {
+    stateCowsay.setState({ text: ev.target.value })
+  }
+  _onChange_mode = val => {
+    stateCowsay.setState({ mode: val })
+  }
+  _onChange_action = val => {
+    stateCowsay.setState({ action: val })
+  }
+  _onChange_eyes = ev => {
+    stateCowsay.setState({ eyes: ev.target.value })
+  }
+  _onChange_tongue = ev => {
+    stateCowsay.setState({ tongue: ev.target.value })
+  }
+  _onSubmit = () => {}
 
-//     let options: any = {
-//       text,
-//     }
-//     if (mode) {
-//       options[mode] = true
-//     }
+  _onClick_share = () => {
+    stateCowsay.doShare()
+  }
 
-//     return (
-//       <div>
-//         {/* <div className={styles.cowForm}>
-//           <Form onSubmit={this._onSubmit}>
-//             <Form.Item label="Text">
-//               <Input
-//                 placeholder="What should the cow say?"
-//                 value={state.text}
-//                 onChange={this._onChange_text}
-//               />
-//             </Form.Item>
-//             <Form.Item label="Mode">
-//               <Select
-//                 defaultValue="b"
-//                 style={{ width: 120 }}
-//                 onChange={this._onChange_mode}
-//               >
-//                 {modes.map(c => (
-//                   <Option value={c.key}>{c.label}</Option>
-//                 ))}
-//               </Select>
-//             </Form.Item>
+  render() {
+    let state = stateCowsay.getState()
 
-//             <Form.Item>
-//               <Button type="primary">Share!</Button>
-//             </Form.Item>
-//           </Form>
-//         </div> */}
+    return (
+      <div className={styles.cowForm}>
+        <div>
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel}>Text</div>
+            <div className={styles.cowFormItem}>
+              <TextArea
+                placeholder="What should the cow say?"
+                value={state.text}
+                autosize
+                onChange={this._onChange_text}
+              />
+            </div>
+          </div>
 
-//         <div className={styles.cowForm}>
-//           <div>
-//             <div className={styles.cowFormRow}>
-//               <div className={styles.cowFormLabel}>Text</div>
-//               <div className={styles.cowFormItem}>
-//                 <Input
-//                   placeholder="What should the cow say?"
-//                   value={state.text}
-//                   onChange={this._onChange_text}
-//                 />
-//               </div>
-//             </div>
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel}>Action</div>
+            <div className={styles.cowFormItem}>
+              <Select
+                value={state.action}
+                style={{ width: 120 }}
+                onChange={this._onChange_action}
+              >
+                {actions.map(c => (
+                  <Option key={c.key} value={c.key}>
+                    {c.label}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+          </div>
 
-//             <div className={styles.cowFormRow}>
-//               <div className={styles.cowFormLabel}>Mode</div>
-//               <div className={styles.cowFormItem}>
-//                 <Select
-//                   defaultValue="b"
-//                   style={{ width: 120 }}
-//                   onChange={this._onChange_mode}
-//                 >
-//                   {modes.map(c => (
-//                     <Option key={c.key} value={c.key}>
-//                       {c.label}
-//                     </Option>
-//                   ))}
-//                 </Select>
-//               </div>
-//             </div>
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel}>Mode</div>
+            <div className={styles.cowFormItem}>
+              <Select
+                value={state.mode}
+                style={{ width: 120 }}
+                onChange={this._onChange_mode}
+              >
+                {modes.map(c => (
+                  <Option key={c.key} value={c.key}>
+                    {c.label}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+          </div>
 
-//             <div className={styles.cowFormRow}>
-//               <div className={styles.cowFormLabel} />
-//               <div className={styles.cowFormItem}>
-//                 <Button type="primary" onClick={this._onClick_share}>
-//                   Share!
-//                 </Button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel}>Eyes</div>
+            <div className={styles.cowFormItem}>
+              <Input
+                placeholder="Custom Eyes i.e. **"
+                maxLength={2}
+                value={state.eyes}
+                onChange={this._onChange_eyes}
+              />
+            </div>
+          </div>
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel}>Tongue</div>
+            <div className={styles.cowFormItem}>
+              <Input
+                placeholder="Custom Tongue i.e. ()"
+                maxLength={2}
+                value={state.tongue}
+                onChange={this._onChange_tongue}
+              />
+            </div>
+          </div>
 
-// export default CowsayOptions
+          <div className={styles.cowFormRow}>
+            <div className={styles.cowFormLabel} />
+            <div className={styles.cowFormItem}>
+              <Button type="primary" onClick={this._onClick_share}>
+                Share!
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- pages/cowsay.tsx
+++ pages/cowsay.tsx
@@ -2,23 +2,15 @@ import * as React from 'react'
 
 import styles from './cowsay.scss'
 import DisplayCow from '../components/DisplayCow'
+import CowsayOptions from '../components/CowsayOptions'
 // const styles = require('./cowsay.scss')
 
 import Layout from '../components/Layout'
 
-import Button from 'antd/lib/button'
-import Input from 'antd/lib/input'
-const { TextArea } = Input
-import Select from 'antd/lib/select'
-const Option = Select.Option
-import Form from 'antd/lib/form'
-
-import fetch from 'isomorphic-unfetch'
-
 import * as stateCowsay from '../state/stateCowsay'
 import { modes, actions } from '../state/stateCowsay'
 
-class Cowsay extends React.Component<{ userAgent: string }> {
+class Cowsay extends React.Component<{}> {
   subscriptionToken = stateCowsay.subscribe(this)
   componentWillUnmount() {
     stateCowsay.unSubscribe(this.subscriptionToken)
@@ -51,120 +43,9 @@ class Cowsay extends React.Component<{ userAgent: string }> {
 
     return (
       <Layout title="cowsay">
-        {/* <div className={styles.cowForm}>
-          <Form onSubmit={this._onSubmit}>
-            <Form.Item label="Text">
-              <Input
-                placeholder="What should the cow say?"
-                value={state.text}
-                onChange={this._onChange_text}
-              />
-            </Form.Item>
-            <Form.Item label="Mode">
-              <Select
-                defaultValue="b"
-                style={{ width: 120 }}
-                onChange={this._onChange_mode}
-              >
-                {modes.map(c => (
-                  <Option value={c.key}>{c.label}</Option>
-                ))}
-              </Select>
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary">Share!</Button>
-            </Form.Item>
-          </Form>
-        </div> */}
-
-        <div className={styles.cowForm}>
-          <div>
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel}>Text</div>
-              <div className={styles.cowFormItem}>
-                <TextArea
-                  placeholder="What should the cow say?"
-                  value={state.text}
-                  autosize
-                  onChange={this._onChange_text}
-                />
-              </div>
-            </div>
-
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel}>Action</div>
-              <div className={styles.cowFormItem}>
-                <Select
-                  value={state.action}
-                  style={{ width: 120 }}
-                  onChange={this._onChange_action}
-                >
-                  {actions.map(c => (
-                    <Option key={c.key} value={c.key}>
-                      {c.label}
-                    </Option>
-                  ))}
-                </Select>
-              </div>
-            </div>
-
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel}>Mode</div>
-              <div className={styles.cowFormItem}>
-                <Select
-                  value={state.mode}
-                  style={{ width: 120 }}
-                  onChange={this._onChange_mode}
-                >
-                  {modes.map(c => (
-                    <Option key={c.key} value={c.key}>
-                      {c.label}
-                    </Option>
-                  ))}
-                </Select>
-              </div>
-            </div>
-
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel}>Eyes</div>
-              <div className={styles.cowFormItem}>
-                <Input
-                  placeholder="Custom Eyes i.e. **"
-                  maxLength={2}
-                  value={state.eyes}
-                  onChange={this._onChange_eyes}
-                />
-              </div>
-            </div>
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel}>Tongue</div>
-              <div className={styles.cowFormItem}>
-                <Input
-                  placeholder="Custom Tongue i.e. ()"
-                  maxLength={2}
-                  value={state.tongue}
-                  onChange={this._onChange_tongue}
-                />
-              </div>
-            </div>
-
-            <div className={styles.cowFormRow}>
-              <div className={styles.cowFormLabel} />
-              <div className={styles.cowFormItem}>
-                <Button type="primary" onClick={this._onClick_share}>
-                  Share!
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CowsayOptions />
 
         <DisplayCow options={options} />
-
-        {/* <div className={styles.cowBox}>
-          <pre>{JSON.stringify(state, null, 2)}</pre>
-        </div> */}
       </Layout>
     )
   }
