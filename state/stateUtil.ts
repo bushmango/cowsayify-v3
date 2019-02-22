af7dc927b82872ee95dcd73e43a0e1d6e5605f7f
sbushman <steve@stevebushman.com>
2019-02-22 17:45:35 -0600

    add state management handling

--- state/stateUtil.ts
+++ state/stateUtil.ts
@@ -3,6 +3,17 @@ const { useState, useEffect } = React
 import * as PubSub from 'pubsub-js'
 import { _ } from '../imports/lodash'
 
+let verbose = false
+export function setVerbose(val: boolean) {
+  verbose = val
+}
+
+function log(...x) {
+  if (console && console.log) {
+    console.log(...x)
+  }
+}
+
 export interface IStateManager<T> {
   stateKey: string
   getState: () => T
@@ -27,6 +38,9 @@ export function createStateManager<T>(
     getState,
     setState: (changes: Partial<T>, sync = false) => {
       state = _.assign({}, state, changes)
+      if (verbose) {
+        log('updated ' + stateKey)
+      }
       if (sync) {
         PubSub.publishSync(stateKey)
       } else {
@@ -34,16 +48,27 @@ export function createStateManager<T>(
       }
     },
     subscribe: (component: React.Component) => {
-      return PubSub.subscribe(stateKey, () => {
+      let token = PubSub.subscribe(stateKey, () => {
         component.forceUpdate()
       })
+      if (verbose) {
+        log('subscribed ' + stateKey + '|' + token)
+      }
+      return token
     },
     subscribeHook(callback: (state: T) => any) {
-      return PubSub.subscribe(stateKey, () => {
+      let token = PubSub.subscribe(stateKey, () => {
         callback(getState())
       })
+      if (verbose) {
+        log('subscribed hook ' + stateKey + '|' + token)
+      }
+      return token
     },
     unSubscribe: token => {
+      if (verbose) {
+        log('unsubscribed ' + stateKey + '|' + token)
+      }
       PubSub.unsubscribe(token)
     },
   }
