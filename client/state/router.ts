44f4ce6385dd55fe8523cc85a2d8aba29670164c
sbushman <steve@stevebushman.com>
2019-04-02 16:23:49 -0500

    mostly working

--- client/state/router.ts
+++ client/state/router.ts
@@ -11,44 +11,55 @@ export function navTo(href: string) {
 
 let prefix = ''
 let staticPrefix = ''
+let isLocal = false
 export function getPrefix(origin, pathname) {
   if (typeof window != 'undefined') {
     origin = window.location.origin
     pathname = window.location.pathname
+    if (origin.indexOf('localhost:') !== -1) {
+      isLocal = true
+      prefix = ''
+      staticPrefix = ''
+    } else if (_.startsWith(pathname, '/dev/')) {
+      prefix = '/dev'
+      staticPrefix = s3Bucket + '/dev'
+    } else if (_.startsWith(pathname, '/prod/')) {
+      prefix = '/prod'
+      staticPrefix = s3Bucket + '/prod'
+    } else {
+      prefix = ''
+      staticPrefix = s3Bucket + '/prod'
+    }
   }
-
-  log.x('getPrefix', origin, pathname)
-
-  if (origin.indexOf('localhost:') !== -1) {
-    prefix = ''
-    staticPrefix = ''
-  } else if (_.startsWith(pathname, '/dev/')) {
-    prefix = '/dev'
-    staticPrefix = s3Bucket + '/dev'
-  } else if (_.startsWith(pathname, '/prod/')) {
-    prefix = '/prod'
-    staticPrefix = s3Bucket + '/prod'
-  } else {
-    prefix = ''
-    staticPrefix = s3Bucket + '/prod'
+  if (typeof process != 'undefined') {
+    let stage = process.env.NODE_STAGE
+    if (stage === 'dev') {
+      isLocal = false
+      prefix = '/dev'
+      staticPrefix = s3Bucket + prefix
+      return
+    }
+    if (stage === 'prod') {
+      isLocal = false
+      prefix = '/prod'
+      staticPrefix = s3Bucket + prefix
+      return
+    }
   }
 }
 getPrefix('', '')
 
 export function correctStatic(href: string) {
-  if (typeof window != 'undefined') {
-    if (_.startsWith(window.location.origin, 'http://localhost:')) {
-      return href
-    } else {
-      return staticPrefix + href
-    }
+  if (isLocal) {
+    return href
+  } else {
+    return staticPrefix + href
   }
 }
 
 export function correctHref(href: string) {
   if (_.startsWith(href, '/')) {
     // Add our host
-    // TODO: work on the server?
     href = prefix + href
   }
   return href
